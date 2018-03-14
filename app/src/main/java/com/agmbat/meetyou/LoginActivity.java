c842539a84649d54f7fa197b79d1a12db4b034a0
mchen <mayimchen@gmail.com>
2018-03-14 11:45:35 +0800

    添加登录页面

---
+++ app/src/main/java/com/agmbat/meetyou/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.agmbat.meetyou;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+/**
+ * 登陆界面
+ */
+public class LoginActivity extends FragmentActivity {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        findViewById(R.id.next_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/agmbat/meetyou/SplashActivity.java
+++ app/src/main/java/com/agmbat/meetyou/SplashActivity.java
@@ -4,24 +4,36 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
 import com.agmbat.android.permissions.PermissionArrayAction;
 import com.agmbat.android.permissions.Permissions;
 
 /**
- * Loading界面
+ * 闪屏页面
  */
-public class LoadingActivity extends FragmentActivity {
+public class SplashActivity extends Activity {
 
-    private static final String TAG = LoadingActivity.class.getSimpleName();
+    private Handler mHandler = new Handler();
+
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            finish();
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        }
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loading);
-
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(R.drawable.ic_launcher_background);
+        setContentView(imageView);
         Permissions.request(this, new String[]{
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.READ_PHONE_STATE,
@@ -32,30 +44,33 @@ public class LoadingActivity extends FragmentActivity {
             public void onResult(String[] permissions, boolean[] grantResults) {
                 if (Permissions.checkResult(grantResults)) {
 //                    addFragment();
+                    mHandler.postDelayed(mRunnable, 3000);
                 } else {
-                    showNoPermissionDialog(LoadingActivity.this);
+                    showNoPermissionDialog();
                 }
             }
         });
+    }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        mHandler.removeCallbacks(mRunnable);
     }
 
     /**
      * 显示没有权限的对话框并退出
-     *
-     * @param activity
      */
-    private void showNoPermissionDialog(final Activity activity) {
-        new AlertDialog.Builder(activity)
+    private void showNoPermissionDialog() {
+        new AlertDialog.Builder(this)
                 .setMessage("请允许相关权限")
                 .setCancelable(false)
                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        activity.finish();
+                        finish();
                     }
                 })
                 .show();
     }
-
 }
