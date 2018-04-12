5a5511d90d2ba78fee590865ac0d7e433fa06a8c
mchen <mayimchen@gmail.com>
2018-04-12 14:35:19 +0800

    支持拍照

--- app/src/main/java/com/agmbat/meetyou/settings/EditAvatarActivity.java
+++ app/src/main/java/com/agmbat/meetyou/settings/EditAvatarActivity.java
@@ -1,11 +1,15 @@
 package com.agmbat.meetyou.settings;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.agmbat.android.image.ImageManager;
 import com.agmbat.android.utils.WindowUtils;
+import com.agmbat.imagepicker.ImagePicker;
+import com.agmbat.imagepicker.loader.UILImageLoader;
+import com.agmbat.imagepicker.ui.ImageGridActivity;
 import com.agmbat.isalertview.AlertView;
 import com.agmbat.isalertview.OnItemClickListener;
 import com.agmbat.meetyou.R;
@@ -18,6 +22,11 @@ import butterknife.OnClick;
  * 编辑头像界面
  */
 public class EditAvatarActivity extends Activity {
+
+    /**
+     * 拍照
+     */
+    private static final int REQUEST_CODE_TAKE_PICTURE = 100;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,13 +59,37 @@ public class EditAvatarActivity extends Activity {
     @OnClick(R.id.title_menu_more)
     void onClickMenuMore() {
         new AlertView(null, null, "取消", null,
-                new String[]{"拍照", "从相册中选择", "保存图片"},
+                new String[]{"拍照", "从手机相册选择", "保存图片"},
                 this, AlertView.Style.ActionSheet, new OnItemClickListener() {
             @Override
             public void onItemClick(AlertView view, int position) {
+                if (position == 0) {
+                    takePicture();
+                } else if (position == 1) {
 
+                } else if (position == 2) {
+
+                }
             }
         }).setCancelable(true).show();
     }
 
+
+    /**
+     * 拍照
+     */
+    private void takePicture() {
+        ImagePicker.getInstance().setImageLoader(new UILImageLoader());
+        Intent intent = new Intent(this, ImageGridActivity.class);
+        intent.putExtra(ImageGridActivity.EXTRAS_TAKE_PICKERS, true); // 是否是直接打开相机
+        startActivityForResult(intent, REQUEST_CODE_TAKE_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_TAKE_PICTURE) {
+
+        }
+    }
 }
